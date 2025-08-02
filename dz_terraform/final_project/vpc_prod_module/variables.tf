905f34a86d9738715d961c1c50f14d689d06f8e7
Sem20071 <sem2007160@gmail.com>
2025-08-02 09:41:10 +0000

    Создание директории final_project

---
+++ dz_terraform/final_project/vpc_prod_module/variables.tf
@@ -0,0 +1,19 @@
+variable "subnet_name" {
+  type = string
+}
+
+variable "subnet_zone" {
+  type = string
+}
+
+variable "network_id" {
+  type = string
+}
+
+variable "cidr_blocks" {
+  type = list(string)
+}
+
+variable "network_name" {
+  type = string
+}
